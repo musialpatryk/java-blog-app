9d67128b5a53b4a2c2dff0215648c969a9c37ee1
musialpatryk <musial.patryk10@gmail.com>
2025-01-03 14:21:03 +0100

    separate views from layout common components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
-import { LoggedInLayoutComponent } from './views/logged-in-layout/logged-in-layout.component';
+import { LoggedInLayoutComponent } from './components/logged-in-layout/logged-in-layout.component';
 import { LoginComponent } from './views/login/login.component';
-import { LoggedOutLayoutComponent } from './views/logged-out-layout/logged-out-layout.component';
+import { LoggedOutLayoutComponent } from './components/logged-out-layout/logged-out-layout.component';
 import { RegisterComponent } from './views/register/register.component';
 
 export const routes: Routes = [
