b964cbff8325bfe0aaae4ea18e55d6cc2671261f
musialpatryk <musial.patryk10@gmail.com>
2025-01-04 20:31:14 +0100

    add basic post design

--- src/app/modules/posts/components/post/post.component.ts
+++ src/app/modules/posts/components/post/post.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 import { IRawPost } from '../../posts.interface';
+import { TranslatePipe } from '../../../translations/pipes/translate.pipe';
 
 @Component({
   selector: 'app-post',
   imports: [
     JsonPipe,
+    TranslatePipe,
   ],
   templateUrl: './post.component.html',
 })
